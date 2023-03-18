6ccbbfb2d5e94a37c4e07da160cdb7a5101fb8dc
Doru <doru.ldf@gmail.com>
2023-03-18 22:30:59 +0200

    Add initial spec for api

--- README.md
+++ README.md
@@ -2,23 +2,18 @@
 
 Final project for ItSchool course, Python - Intro To Programming.
 
-# Finance API
-
-Final project for ItSchool course, Python - Intro To Programming.
-
 For Windows, steps to deploy:
 
 ''' 
-git clone <git_repo_url>
-cd itschhool3-finance-project
+git clone <git_repo_url> 
+cd itschool3-finance-project
 python3 -m venv env/
 ./env/Scripts/activate
 pip install --upgrade pip
 pip install -r requirements.txt
 
-
 '''
 
-this project uses FastAPI & uvicorn.
-FastAPI docs: https://fastapi.tiangolo.com/
+This project uses FastAPI & uvicorn.
+FastAPI docs: https://fastapi.tiangolo.com/   
 
--- finance-project/main.py
+++ finance-project/main.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI
 app = FastAPI(
     debug=True,
     title="Fintech Portofolio API",
-    description="A web server with a REST API for keeping track of your different financial assets,"
+    description="A webserver with a REST API for keeping track of your different financial assets,"
     " stocks & crypto, and see/compare their evolution",
     version="0.1.0",
 )
@@ -11,4 +11,4 @@ app = FastAPI(
 if __name__ == "__main__":
     import subprocess
 
-    subprocess.run(["uvicorn", "finance-project.main:app", "--reload"])
+subprocess.run(["uvicorn", "finance-project.main:app", "--reload"])
