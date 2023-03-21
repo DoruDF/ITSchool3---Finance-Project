e6de2e49304aa0220553e3c723508a4bd20f4c3b
Lucacela <m.lucacela@yahoo.com>
2023-03-21 20:00:38 +0200

    Add factory for user

--- tests/domain/user/test_user.py
+++ tests/domain/user/test_user.py
@@ -13,6 +13,13 @@ class UserMyTestCase(unittest.TestCase):
         # asertion
         self.assertEqual(username, actual_username)
 
+    def test_it_gets_empty_list_if_we_do_not_specify_stock(self):
+        user = User("random-username")
+
+        actual_stocks = user.stocks
+
+        self.assertEqual([], actual_stocks)
+
 
 if __name__ == "__main__":
     unittest.main()
