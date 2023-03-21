befe0569add5db5901940df05278b95c1c60d65b
Gradinariu Emilian <gradinariuemilian@gmail.com>
2023-03-21 20:00:41 +0200

    Add factory for users

--- tests/domain/user/test_user.py
+++ tests/domain/user/test_user.py
@@ -1,22 +1,27 @@
 import unittest
 from domain.user.user import User
-class UserMyTestCase(unittest.TestCase):
 
+
+class UserMyTestCase(unittest.TestCase):
     def test_user_sets_the_right_username(self):
-        #set up
+        # set up
         username = "random generated"
         user = User(username)
-        actual_username= user.username
+        actual_username = user.username
         self.assertEqual(username, actual_username)
-    @unittest.skip("TODO: Homework")
+
     def test_it_sets_empty_list_if_we_do_not_specify_stock(self):
-        pass
+        username = User("random user")
+        wanted_stocks = []
+        actual_stocks = username.stocks
+        self.assertEqual(wanted_stocks, actual_stocks)
 
-    @unittest.skip("TODO: Homework")
     def test_it_sets_the_stocks_we_give(self):
-        pass
+        given_stocks = ["SPX", "RASDAQ", "MOT", "PEX"]
+        username = User("random user", given_stocks)
+        actual_stocks = username.stocks
+        self.assertEqual(given_stocks, actual_stocks)
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
