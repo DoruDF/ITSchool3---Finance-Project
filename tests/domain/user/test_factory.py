c3b033348fa909a3998562aca24ad09885a6d153
Lucacela <m.lucacela@yahoo.com>
2023-03-23 16:01:29 +0200

    Add repo for user

--- tests/domain/user/test_factory.py
+++ tests/domain/user/test_factory.py
@@ -5,8 +5,8 @@ from domain.user.user import User
 
 
 class UserFactoryTestCase(unittest.TestCase):
-    def test_it_creates_a_user_if_the_username_is_between_6_and_20_charts(self):
-        username = "beetween-6-and-20"
+    def test_it_creates_a_user_if_the_username_is_between_6_and_20_chars(self):
+        username = "between-6-and-20"
         factory = UserFactory()
 
         actual_user = factory.make(username)
