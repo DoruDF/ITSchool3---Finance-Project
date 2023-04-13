b78436b14b91b7d32620f5fbcb474dec2dcbc1f3
AnaMaria <anaungureanu80@yahoo.com>
2023-04-13 20:59:39 +0300

    Add task for cleaning up graphs

--- finance-project/main.py
+++ finance-project/main.py
@@ -1,5 +1,9 @@
 import logging
 from fastapi import FastAPI, Request
+from fastapi_utils.tasks import repeat_every
+import subprocess
+import os
+import time
 from api.users import users_router
 from api.assets import assets_router
 from domain.user.factory import InvalidUsername
@@ -14,7 +18,6 @@ logging.basicConfig(
 app = FastAPI(
     debug=True,
     title="Fintech Portfolio API",
-    # TODO add to README
     description="A webserver with a REST API for keeping track of your different financial assets,"
     " stocks & crypto, and see/compare their evolution",
     version="0.3.0",
@@ -25,11 +28,25 @@ app.include_router(assets_router)
 
 @app.exception_handler(InvalidUsername)
 def return_invalid_username(_: Request, e: InvalidUsername):
-    return JSONResponse(status_code=400, content="Username is not valid! Error: " + str(e))
+    return JSONResponse(
+        status_code=400, content="Username is not valid! Error: " + str(e)
+    )
+
+
+@app.on_event("startup")
+@repeat_every(seconds=60 * 60 * 24)
+def clean_images_older_than_24h():
+    files = os.listdir(".")
+    graphs = [f for f in files if f.endswith(".png")]
+    current_posix_time = time.time()
+    ago_24h = current_posix_time - 60 * 60 * 24
+    for g in graphs:
+        g_creation_time = os.path.getctime(g)
+        if g_creation_time < ago_24h:
+            os.remove(g)
 
 
 if __name__ == "__main__":
-    import subprocess
     logging.info("Starting webserver ...")
     try:
         subprocess.run(["uvicorn", "finance-project.main:app", "--reload"])
