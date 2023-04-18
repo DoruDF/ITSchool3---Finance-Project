04cf6f8e9710d0c48082699df14f243a0f93030f
Doru <doru.ldf@gmail.com>
2023-04-18 15:55:33 +0300

    Add api endpoint to insert stocks into a user's portfolio

--- finance-project/api/models.py
+++ finance-project/api/models.py
@@ -1,14 +1,47 @@
 from pydantic import BaseModel, Field
+from uuid import UUID
 
 
-class UserAdd(BaseModel):
-    username: str = Field(description="Alphanumeric username between 6 and 20 chars")
-
-
-class UserInfo(BaseModel):
-    username: str
-    stocks: list[str]
-
+class OrmModel(BaseModel):
     class Config:
         orm_mode = True
 
+
+class UserAdd(BaseModel):
+    username: str = Field(
+        description="Alphanumeric username between 6 and 20 characters"
+    )
+
+
+class AssetAdd(BaseModel):
+    ticker: str
+
+
+class AssetInfoBase(OrmModel):
+    ticker: str
+    name: str
+    country: str
+    sector: str
+
+
+class AssetInfoUser(AssetInfoBase):
+    units: float
+
+
+class AssetInfoPrice(AssetInfoBase):
+    current_price: float
+    currency: str
+    today_low_price: float
+    today_high_price: float
+    open_price: float
+    closed_price: float
+    fifty_day_price: float
+    percentage_difference_between_closed_and_current_price: str
+
+
+class UserInfo(OrmModel):
+    id: UUID = Field(
+        description="ID by which to identify a specific USER",
+    )
+    username: str
+    stocks: list[AssetInfoBase]
