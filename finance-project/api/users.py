4b266379bd400305cefe19c3b565e3e2c8898f98
Doru <doru.ldf@gmail.com>
2023-04-18 16:00:33 +0300

    ADD PLOTS & SINGLETON

--- finance-project/api/users.py
+++ finance-project/api/users.py
@@ -1,26 +1,42 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 
+from domain.asset.factory import AssetFactory
+from domain.asset.repo import AssetRepo
 from domain.user.repo import UserRepo
 from domain.user.factory import UserFactory
-from api.models import UserAdd, UserInfo
+from api.models import UserAdd, UserInfo, AssetInfoUser, AssetAdd
+from persistence.user_file import UserPersistenceFile
+from persistence.user_sqlite import UserPersistenceSqlite
 
 users_router = APIRouter(prefix="/users")
 
-repo = UserRepo("main_users.json")
+
+def get_user_repo() -> UserRepo:
+    # user_persistence = UserPersistenceFile("main_users.json")
+    user_persistence = UserPersistenceSqlite()
+    return UserRepo(user_persistence)
 
 
 @users_router.get("", response_model=list[UserInfo])
-def get_all_users():
+def get_all_users(repo=Depends(get_user_repo)):
     return repo.get_all()
 
 
-@users_router.get("/{username}", response_model=UserInfo)
-def get_user(username: str):
-    return repo.get_by_username(username)
+@users_router.get("/{user_id}", response_model=UserInfo)
+def get_user(user_id: str, repo=Depends(get_user_repo)):
+    return repo.get_by_id(user_id)
 
 
-@users_router.post("")
-def create_a_user(new_user: UserAdd):
-    user = UserFactory().make(new_user.username)
+@users_router.post("", response_model=UserInfo)
+def create_a_user(new_user: UserAdd, repo=Depends(get_user_repo)):
+    user = UserFactory().make_new(new_user.username)
     repo.add(user)
+    return user
 
+
+@users_router.post("/{user_id}/assets", response_model=AssetInfoUser)
+def add_asset_to_user(user_id: str, asset: AssetAdd, repo=Depends(get_user_repo)):
+    new_asset = AssetFactory().make_new(asset.ticker)
+    user = repo.get_by_id(user_id)
+    AssetRepo().add_to_user(user, new_asset)
+    return new_asset
